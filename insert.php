a19bf5a000ab3e15c7b955ba793e3e4ea3ee4a81
ZoeQi <qiyiru@gmail.com>
2017-02-09 20:46:44 -0500

    Update insert.php
    multiple inserttions

--- insert.php
+++ insert.php
@@ -1,14 +1,28 @@
 <?php 
 require "conn.php";
-$event_id = $_POST["event_id"];
-$event_type = $_POST["event_type"];
-$mysql_qry = "INSERT INTO event (id, type, timestamp) VALUES ('$event_id','$event_type', NOW());";
 
-if($conn->query($mysql_qry) === TRUE) {
-echo " Insert success";
+$event_type = $_POST["playList"];
+
+if(empty($event_type)) 
+{
+	echo("No plays selected.");
+} 
+else
+{
+	$N = count($event_type);
+	echo("You selected $N play(s): ");
+	
+	for($i=0; $i < $N; $i++){
+		$mysql_qry = "INSERT INTO event (id, type, timestamp) VALUES (NULL, $event_type[$i], NOW());";
+		if($conn->query($mysql_qry) === TRUE) {
+			echo " Insert success";
+		}
+		else {
+			echo " insert not success";
+		}
+	}
 }
-else {
-echo " insert not success";
-}
+
+
 $conn->close; 
 ?>
