74d55688b928f215b6f1011978c62e4f77b5094b
Wong Yong Jie <yjwong92@gmail.com>
2017-03-21 01:34:59 -0400

    Made database connection code aware of environment variables.

--- conn.php
+++ conn.php
@@ -1,9 +1,9 @@
 <?php
-$db_name= "playbook";
-$mysql_username= "root";
-$mysql_pswd ="";
-$server_name="localhost";
-$conn= mysqli_connect($server_name,$mysql_username,$mysql_pswd,$db_name);
+$db_name = isset($_ENV['DB_NAME']) ? $_ENV['DB_NAME'] : 'playbook';
+$db_username = isset($_ENV['DB_USERNAME']) ? $_ENV['DB_USERNAME'] : 'root';
+$db_password = isset($_ENV['DB_PASSWORD']) ? $_ENV['DB_PASSWORD'] : '';
+$db_host = isset($_ENV['DB_HOST']) ? $_ENV['DB_HOST'] : 'localhost';
+$conn = mysqli_connect($db_host, $db_username, $db_password, $db_name);
 if($conn)
 {
 echo "Connection success <br> <br>";
