0eb7d2a6b690006a0bf5b9a41338971757122d4e
ZoeQi <qiyiru@gmail.com>
2017-03-30 18:41:05 -0400

    submit score

--- src/routes.php
+++ src/routes.php
@@ -14,10 +14,12 @@ $app->get('/events', function ($request, $response) {
 $app->post('/events', function ($request, $response) {
 	$parsedBody = $request->getParsedBody();
 	$sectionId = $parsedBody['id'];
+	$userId = $parsedBody['userId'];
+	$userName = $parsedBody['userName'];
 	//$seatNo = $parsedBody['SeatNo'];
-	$insertStatement = $this->database->insert(array('SeatNo', 'UserName', 'SecId'))
+	$insertStatement = $this->database->insert(array('UserName', 'UserId', 'SecId'))
 									->into('players')
-									->values(array('noneedanymore', 'Zoe?', $sectionId));
+									->values(array($userName, $userId, $sectionId));
 	$insertId = $insertStatement->execute(false);
 	//print_r($request->getQueryParam('stadiumSeatNumber'));
 	//print_r($parsedBody['stadiumSeatNumber']);
@@ -31,6 +33,21 @@ $app->post('/score', function ($request, $response) {
 	$selectStatement = $this->database->select(array('SecScore'))
 									  ->from('sections')
 									  ->where('id', '=', $sectionId);
+	$stmt = $selectStatement->execute();
+	$data = $stmt->fetch();
+	return $response->withJson($data);
+	//print_r($request->getQueryParam('stadiumSeatNumber'));
+	//print_r($parsedBody['stadiumSeatNumber']);
+	//return $data;
+});
+
+$app->post('/selfScore', function ($request, $response) {
+	$parsedBody = $request->getParsedBody();
+	$userId = $parsedBody['id'];
+	$selectStatement = $this->database->select(array('PredictionScore', 'CollectionScore', '(PredictionScore+CollectionScore) AS Total'))
+									  ->from('players')
+									  ->where('UserId', '=', $userId)
+									  ;
 	$stmt = $selectStatement->execute();
 	$data = $stmt->fetch();
 	return $response->withJson($data);
@@ -59,6 +76,68 @@ $app->get('/highest', function ($request, $response) {
 	//return $data;
 });
 
+$app->get('/leaderboard', function ($request, $response) {
+	//$parsedBody = $request->getParsedBody();
+	//$sectionId = $parsedBody['id'];		
+
+	//print_r($high);				  
+
+	$selectStatement = $this->database->select(array('UserName', 'PredictionScore', 'CollectionScore', '(PredictionScore+CollectionScore) AS Total'))
+									  ->from('players')
+									  ->orderBy('Total', 'DESC')
+									  ->limit(10,0)
+									//  ->where('SecScore', '=', $high)
+									  ;
+	//$selectStatement->having('MAX(SecScore)', '<', 10000);								  
+	$stmt = $selectStatement->execute();
+	$data = $stmt->fetchAll();
+	return $response->withJson($data);
+	//print_r($request->getQueryParam('stadiumSeatNumber'));
+	//print_r($parsedBody['stadiumSeatNumber']);
+	//return $data;
+});
+
+$app->get('/leaderboardP', function ($request, $response) {
+	//$parsedBody = $request->getParsedBody();
+	//$sectionId = $parsedBody['id'];		
+
+	//print_r($high);				  
+
+	$selectStatement = $this->database->select(array('UserName', 'PredictionScore', 'CollectionScore', '(PredictionScore+CollectionScore) AS Total'))
+									  ->from('players')
+									  ->orderBy('PredictionScore', 'DESC')
+									  ->limit(10,0)
+									//  ->where('SecScore', '=', $high)
+									  ;
+	//$selectStatement->having('MAX(SecScore)', '<', 10000);								  
+	$stmt = $selectStatement->execute();
+	$data = $stmt->fetchAll();
+	return $response->withJson($data);
+	//print_r($request->getQueryParam('stadiumSeatNumber'));
+	//print_r($parsedBody['stadiumSeatNumber']);
+	//return $data;
+});
+
+$app->get('/leaderboardC', function ($request, $response) {
+	//$parsedBody = $request->getParsedBody();
+	//$sectionId = $parsedBody['id'];		
+
+	//print_r($high);				  
+
+	$selectStatement = $this->database->select(array('UserName', 'PredictionScore', 'CollectionScore', '(PredictionScore+CollectionScore) AS Total'))
+									  ->from('players')
+									  ->orderBy('CollectionScore', 'DESC')
+									  ->limit(10,0)
+									//  ->where('SecScore', '=', $high)
+									  ;
+	//$selectStatement->having('MAX(SecScore)', '<', 10000);								  
+	$stmt = $selectStatement->execute();
+	$data = $stmt->fetchAll();
+	return $response->withJson($data);
+	//print_r($request->getQueryParam('stadiumSeatNumber'));
+	//print_r($parsedBody['stadiumSeatNumber']);
+	//return $data;
+});
 
 $app->post('/move', function ($request, $response) {
 				  
@@ -74,6 +153,44 @@ $app->post('/move', function ($request, $response) {
 	//print_r($parsedBody['stadiumSeatNumber']);
 	//return $data;
 });
+
+$app->post('/updateScore', function ($request, $response) {
+				  
+	$parsedBody = $request->getParsedBody();
+	$userId = $parsedBody['id'];
+	$cat = $parsedBody['cat'];
+	if($cat == "predict"){
+		$predictionScore = $parsedBody['predictScore'];
+		$updateStatement = $this->database->prepare("UPDATE players
+												SET PredictionScore = PredictionScore + :predictScore
+												WHERE UserId = :userId");
+		$updateStatement->bindParam(':predictScore', $predictionScore, PDO::PARAM_INT);
+        $updateStatement->bindParam(':userId', $userId, PDO::PARAM_STR);
+		/*$updateStatement = $this->database->update(array('PredictionScore'))
+									  ->set(array('PredictionScore' => 'PredictionScore' + $predictionScore))
+									  ->table('players')
+									  ->where('UserId', '=', $userId);
+									  */
+	}
+	else if ($cat == "collect"){
+		$collectionScore = $parsedBody['collectScore'];
+		$updateStatement = $this->database->prepare("UPDATE players
+												SET CollectionScore = CollectionScore + :collectScore
+												WHERE UserId = :userId");
+		$updateStatement->bindParam(':collectScore', $collectionScore, PDO::PARAM_INT);
+        $updateStatement->bindParam(':userId', $userId, PDO::PARAM_STR);
+	}
+	else{
+		echo "Wrong category";
+	}
+	$stmt = $updateStatement->execute();
+	//$data = $stmt->fetch();
+	return $response;
+	//print_r($request->getQueryParam('stadiumSeatNumber'));
+	//print_r($parsedBody['stadiumSeatNumber']);
+	//return $data;
+});
+
 
 /*
 $app->get('/[{name}]', function ($request, $response, $args) {
